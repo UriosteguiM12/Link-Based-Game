93f408f08dbcaa9bac8c29e2b45e6cb681291b07
UriosteguiM12 <marinauriostegui88@gmail.com>
2025-04-21 20:45:26 -0700

    first version

--- rules.js
+++ rules.js
@@ -1,36 +1,109 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
         this.engine.addChoice("Begin the story");
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location, this.engine.storyData.InitialLocation); // TODO: replace this text by the initial location of the story
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
-        
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+        let locationData = this.engine.storyData.Locations[key];
+
+        if (key === "Exit") {
+            const walkieData = this.engine.storyData.Locations["Walkies"];
+            const managerData = this.engine.storyData.Locations["Manager"];
+    
+            const hasTrueKey = walkieData && walkieData.TrueKey;
+            const hasFakeKey = managerData && managerData.FakeKey;
+    
+            if (hasTrueKey) {
+                this.engine.show(locationData.Body3); // Escaped successfully
+                if (locationData.Escape) {
+                    for (let choice of locationData.Escape) {
+                        this.engine.addChoice(choice.Text, choice);
+                    }
+                } else {
+                    this.engine.addChoice("The end.");
+                }
+            } else if (hasFakeKey) {
+                this.engine.show(locationData.Body2); // Tried wrong key
+                this.engine.addChoice("[ Go to the cashier area ]", { Text: "[ Go to the cashier area ]", Target: "Walkies" });
+            } else {
+                this.engine.show(locationData.Body); // No key yet
+                locationData.Flag = true;
+                this.engine.addChoice("[ Go to the dining area ]", { Text: "[ Go to the dining area ]", Target: "Dining" });
+            }
+    
+            return;
+        }
+    
+        if (locationData.Flag) {
+            this.engine.show(locationData.Body2);
+        } else {
+            this.engine.show(locationData.Body);
+            locationData.Flag = true;
+        }
+    
+        if (locationData.Choices) {
+            for (let choice of locationData.Choices) {
+                this.engine.addChoice(choice.Text, choice);
             }
         } else {
-            this.engine.addChoice("The end.")
+            this.engine.addChoice("The end.");
         }
     }
 
     handleChoice(choice) {
-        if(choice) {
-            this.engine.show("&gt; "+choice.Text);
-            this.engine.gotoScene(Location, choice.Target);
+        if (choice) {
+            this.engine.show("&gt; " + choice.Text);
+            let nextLocationKey = choice.Target;
+    
+            if (nextLocationKey === "Walkies") {
+                this.engine.gotoScene(Walkies, nextLocationKey);
+            } else {
+                this.engine.gotoScene(Location, nextLocationKey);
+            }
         } else {
             this.engine.gotoScene(End);
         }
+    }
+}
+    
+
+class Walkies extends Location {
+    create(key) {
+        let locationData = this.engine.storyData.Locations[key];
+
+        // Show the correct body text based on key obtained
+        if (locationData.TrueKey) {
+            this.engine.show(locationData.Body3);
+        } else if (locationData.Flag) {
+            this.engine.show(locationData.Body2);
+        } else {
+            this.engine.show(locationData.Body);
+            locationData.Flag = true;
+        }
+
+        // Show choices
+        if (locationData.Choices) {
+            for (let choice of locationData.Choices) {
+                this.engine.addChoice(choice.Text, choice);
+            }
+        } else {
+            this.engine.addChoice("The end.");
+        }
+    }
+
+    handleChoice(choice) {
+        if (choice && choice.Text === "[ Channel 3 ]") {
+            this.engine.storyData.Locations["Walkies"].TrueKey = true; // Save that the player now has the key
+        }
+
+        super.handleChoice(choice);
     }
 }
 
